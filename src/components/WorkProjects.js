7f16c6a6a9e44bed016889f9617d910bee72b05d
omarkhris <68520631+omarkhris@users.noreply.github.com>
2024-04-06 18:00:42 -0500

    pushing changes

--- src/components/WorkProjects.js
+++ src/components/WorkProjects.js
@@ -1,23 +1,44 @@
-import { Link } from "react-router-dom"
-import "./WorkProjects.css"
-
-import React from 'react'
+import React, { useState } from 'react';
 
 const WorkProjects = (props) => {
+    const [expanded, setExpanded] = useState(false);
+
+    const toggleExpand = () => {
+        setExpanded(!expanded);
+    };
+
+    const navigateToWebsite = (url) => {
+        window.open(url, '_blank');
+    };
+
     return (
-        <div className="container">
-            <img src={props.imgsrc} alt="image" />
-            <h2 className="project-title">{props.title}</h2>
-            <div className="card-details">
-                <p>{props.text}</p>
-                <div className="card-btns">
-                    <Link to={props.view} className="btn">
-                     repo 
-                    </Link>
+        
+            <div className="container">
+                <img src={props.imgsrc} alt="image" />
+                <h2 className="project-title">{props.title}</h2>
+                <div className="card-details">
+                    <p>{expanded ? props.text : `${props.text.slice(0, 150)}...`}</p>
+                    {props.text.length > 150 && (
+                        <button onClick={toggleExpand} className="btn">
+                            {expanded ? 'Read less' : 'Read more'}
+                        </button>
+                    )}
+                    <div className="card-btns">
+                        {props.view && (
+                            <a href={props.view} className="btn" target="_blank" rel="noopener noreferrer">
+                                View Repo
+                            </a>
+                        )}
+                        {props.website && (
+                            <button onClick={() => navigateToWebsite(props.website)} className="btn" style={{ marginTop: '13px' }}>
+                                Visit
+                            </button>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        
+    );
+};
 
-export default WorkProjects
+export default WorkProjects;
--- src/components/WorkProjectsData.js
+++ src/components/WorkProjectsData.js
@@ -2,14 +2,24 @@ import pro1 from "../assets/proper.png"
 import pro2 from "../assets/project2.png"
 import port from '../assets/portfo.png'
 import badge from '../assets/badge .png'
+import moviePage from "../assets/Screenshot 2024-04-06 161838.png"
+import bankAPP from "../assets/maxresdefault.jpg"
 
 
 const DisplayData = [
+    {
+        imgsrc: moviePage,
+        title: "Movie Theater",
+        text: "Our movie discovery platform utilizes React for the frontend, hosted on AWS Amplify, and Spring Boot for the backend, containerized with Docker and deployed on an EC2 instance. Traffic is efficiently managed with Amazon CloudFront and Route 53. MongoDB handles data storage and retrieval, ensuring smooth performance.",
+        view:"https://github.com/omarkhris/MovieFrontEnd",
+        website:"https://main.d128ecqayn2zlp.amplifyapp.com/"
+    },  
     {
         imgsrc: pro1,
         title: "PropertyList Trade",
         text: "Property Management Website built for users to publish, explore, bid, and sell properties online. Project was developed using a 3-tier architecture, utilizing Spring-boot, Hibernate, Spring data JPA, React.js, Postgres SQL, and JWT token. I implemented the JWT token to authorize activeties for different users on the platform, along with the property service integrated with front-end interface. During an on-campus course at Maharishi Interntional University.",
         view: "https://github.com/omarkhris/WAA-Final-project",
+        website:""
     },
     {
         imgsrc: pro2,
@@ -17,28 +27,32 @@ const DisplayData = [
         text: "E-commerce website course project implemented to demonstrate how to design flexible, scalable, testable, and resilient software systems using microservices. Developed product service and user service, containerized to run on a Kubernetes cluster and integrated to operate independently and loosely coupled. Leveraged tools including Java, Spring Boot, Spring (Hibernate, Cloud, Eureka, Security), MongoDB, MySQL, Docker, and Azure Cloud."
         ,
         view: "https://github.com/omarkhris/SA-WardrobeWiz",
+        website:""
     },
     {
         imgsrc: port,
         title: "Personal Portfolio",
         text: " The Personal Portfolio Website is a dynamic web application developed using React.js and hosted on the Amazon Web Services (AWS) platform. This project serves as a showcase of an individual's skills, projects, and experiences, providing a professional online presence to attract potential employers, clients, or collaborators. The portfolio website leverages several AWS services to enhance its functionality and scalability, including: Amazon S3 Bucket, CloudFront, Lambda, Route 53, EC2, API Gateway, API Gateway, ECS, Auto Scaling Group, SNS."
         ,
-        view: "https://www.omarkhrissat.com/",
+        view: "https://github.com/omarkhris/portfolio",
+        website:"https://www.omarkhrissat.com/"
     },
 
     {
         imgsrc: badge,
-        title: "Badge & Membership System",
+        title: "Badge & Membership ",
         text: " The Badge & Membership System is a RESTful application designed to manage access control within a university environment, catering to a diverse community comprising thousands of students, staff, and faculty. The system revolves around the concept of \"badges\" that uniquely identify each member and \"memberships\" that grant access to various university facilities and services. Features: Badge Management, Membership Control, Types of Memberships, Access Authorization. Technologies Used: Java, Spring Framework, Database Management System, Authentication and Authorization.",
         view: "https://github.com/omarkhris/EAProjectMyWork",
+        website:""
     },
     
     {
-        imgsrc: pro1,
+        imgsrc: bankAPP,
         title: "Bank System",
         text: " Implemented a simple bank system to demonstrate and apply spring key concepts of Aspect-Oriented programming, Dependency Injection, Data Access Objects and Java Message Service. " +
             "Also comprehensive logging strategy using SLF4J and Logback enhanced monitoring and debugging capabilities.",
         view: "https://github.com/omarkhris/bankSystem",
+        website:""
     }
 ]
 
